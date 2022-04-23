bcdfbd90485c5f10a6f704143edb0c2f55024e65
Nyobabot011 <101327761+Nyobabot011@users.noreply.github.com>
2022-04-23 19:21:38 +0700

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -79,7 +79,7 @@ def get_reply_markup(username, query):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [
         [
-            InlineKeyboardButton('Cari Lagi', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('Cari Anime', switch_inline_query_current_chat=query),
             InlineKeyboardButton('Bagikan Bot', url=url),
         ]
     ]
