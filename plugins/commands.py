128f77106435c248d140c865711f6119f6f2305e
Nyobabot011 <101327761+Nyobabot011@users.noreply.github.com>
2022-04-23 22:30:01 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,7 +20,7 @@ async def start(bot, message):
             InlineKeyboardButton('🔗 Bagikan Bot', switch_inline_query=''),
             ],
             [
-            InlineKeyboardButton("❓ Cara Pakai Botnya",  callback_data="help"),
+            InlineKeyboardButton("❓ Cara Pakai Botnya", callback_data="help"),
             ],
           ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -112,5 +112,5 @@ async def bot_info(bot, message):
             InlineKeyboardButton('Request Anime', url='https://gawr.floral.workers.dev/#contact'),
         ]
         ]
-    await message.reply(text="<b>Cara Pencarian</b>/n/n➤ Gunakan kata kunci untuk mempermudah pencarian: kata kunci [search query]. Contoh: GP eyes/n/n➤ kata kunci: Gawrproject, GP, kusonime, kuso, wibudesu, animebatch, himitsukaze, moenime, dll./n/n➤ Kalian bisa hubungi owner jika ada kendala dengan bot atau request anime dengan menekan tombol di bawah.", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>Cara Pencarian.</b>\n\n➤ Gunakan kata kunci untuk mempermudah pencarian: kata kunci [search query]. Contoh: GP eyes.\n\n➤ kata kunci: Gawrproject, GP, kusonime, kuso, wibudesu, animebatch, himitsukaze, moenime, dll.\n\n➤ Kalian bisa hubungi owner jika ada kendala dengan bot atau request anime dengan menekan tombol di bawah.", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
